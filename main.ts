8567ba5bc3a07b69f060662a6406513f4056b335
alex-liping <157304277+alex-liping@users.noreply.github.com>
2024-01-30 17:38:31 +0100

    mirne doleva a doprava

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
+function doprava_mirne () {
+    kBit.motor(KBitMotorObs.RightSide, KBitMotorDir.Forward, 40)
+    kBit.motor(KBitMotorObs.LeftSide, KBitMotorDir.Forward, 60)
+}
 function doprava () {
-    kBit.motor(KBitMotorObs.LeftSide, KBitMotorDir.Forward, 20)
-    kBit.motor(KBitMotorObs.RightSide, KBitMotorDir.Back, 20)
+    kBit.motor(KBitMotorObs.LeftSide, KBitMotorDir.Forward, 30)
+    kBit.motor(KBitMotorObs.RightSide, KBitMotorDir.Back, 30)
     basic.pause(2000)
     kBit.motorStop(KBitMotorObs.LeftSide)
     kBit.motorStop(KBitMotorObs.RightSide)
+}
+function doleva_mirne () {
+    kBit.motor(KBitMotorObs.LeftSide, KBitMotorDir.Forward, 40)
+    kBit.motor(KBitMotorObs.RightSide, KBitMotorDir.Forward, 60)
 }
 input.onButtonPressed(Button.A, function () {
     kBit.carStop()
@@ -11,20 +19,26 @@ input.onButtonPressed(Button.A, function () {
 function couvni () {
     kBit.carStop()
     basic.pause(500)
-    kBit.run(KBitDir.RunBack, 15)
+    kBit.run(KBitDir.RunBack, 20)
     basic.pause(1000)
     kBit.carStop()
 }
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 1000; index++) {
-        if (kBit.ultra() > 8) {
+        if (kBit.ultra() > 10) {
             kBit.run(KBitDir.RunForward, 50)
+            if (kBit.obstacle(KBitMotorObs.LeftSide) == 0) {
+                doprava_mirne()
+            }
+            if (kBit.obstacle(KBitMotorObs.RightSide) == 0) {
+                doleva_mirne()
+            }
         } else {
             couvni()
-            if (kBit.obstacle(KBitMotorObs.LeftSide) == 0) {
+            if (kBit.obstacle(KBitMotorObs.LeftSide) == 1) {
                 doleva()
             } else {
-                if (kBit.obstacle(KBitMotorObs.RightSide) == 0) {
+                if (kBit.obstacle(KBitMotorObs.RightSide) == 1) {
                     doleva()
                 }
             }
@@ -32,8 +46,8 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 function doleva () {
-    kBit.motor(KBitMotorObs.LeftSide, KBitMotorDir.Back, 20)
-    kBit.motor(KBitMotorObs.RightSide, KBitMotorDir.Forward, 20)
+    kBit.motor(KBitMotorObs.LeftSide, KBitMotorDir.Back, 30)
+    kBit.motor(KBitMotorObs.RightSide, KBitMotorDir.Forward, 30)
     basic.pause(2000)
     kBit.motorStop(KBitMotorObs.LeftSide)
     kBit.motorStop(KBitMotorObs.RightSide)
